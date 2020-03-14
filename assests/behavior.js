e9b5d72767b63205bfd6413c1030da7467c6d3b8
Zach S.L <zach.s.ledford@gmail.com>
2020-03-14 10:38:09 -0400

    added ajax, resopnse function and console.log results

--- assests/behavior.js
+++ assests/behavior.js
@@ -4,6 +4,23 @@ $(document).ready(function() {
 
     $("button").on("click", function(){
         var anime = $(this).attr("anime-show");
-        console.log(anime)
+        console.log(anime);
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        anime + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+        // AJAX GET request
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response){
+            var results = response.data;
+
+            for (var i =0; i< results.length;i++){
+                console.log(results);
+            }
+        })
+
     })
 });
