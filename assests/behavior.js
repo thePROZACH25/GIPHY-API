0e20e9cbbc93be465ad4b3810452b43fa713b600
Zach S.L <zach.s.ledford@gmail.com>
2020-03-14 10:11:56 -0400

    made on 'click'for button and console.log the first var

--- assests/behavior.js
+++ assests/behavior.js
@@ -1,3 +1,9 @@
+// document.ready function (Loads after the Html is loads)
 $(document).ready(function() {
     console.log("ready!");
+
+    $("button").on("click", function(){
+        var anime = $(this).attr("anime-show");
+        console.log(anime)
+    })
 });
