7f58f6699b7076dcb2264af01edcb620c2842d01
Zach S.L <zach.s.ledford@gmail.com>
2020-03-17 11:44:32 -0400

    added a renderButtons function

--- assests/behavior.js
+++ assests/behavior.js
@@ -1,26 +1,43 @@
 // document.ready function (Loads after the Html is loads)
 $(document).ready(function() {
-    console.log("ready!");
+  console.log("document ready");
 
-    $("button").on("click", function(){
-        var anime = $(this).attr("anime-show");
-        console.log(anime);
+  var topics = ["Dragon Ball", "Pokemon", "Sailor Moon", "One Piece"];
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        anime + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+  function renderButtons() {
+    $("#buttons").empty();
 
-        // AJAX GET request
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function(response){
-            var results = response.data;
+    for (var i = 0; i < topics.length; i++) {
+      var a = $("<button>");
+      a.addClass("anime");
+      a.attr("anime-show", topics[i]);
+      a.text(topics[i]);
+      $("#buttons").append(a);
+    }
+  }
+  console.log(renderButtons);
 
-            for (var i =0; i< results.length;i++){
-                console.log(results);
-            }
-        })
+  // $("button").on("click", function(){
+  //     var anime = $(this).attr("anime-show");
+  //     console.log(anime);
 
-    })
+  // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+  // anime + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+  // // AJAX GET request
+  // $.ajax({
+  //     url: queryURL,
+  //     method: "GET"
+  // })
+  // .then(function(response){
+  //     $("#img-div").text(JSON.stringify(response));
+  //     // var results = response.data;
+
+  //     // for (var i =0; i< results.length;i++){
+  //     //     console.log(results);
+  //     // }
+  //     // })
+  //     renderButtons();
+  // })
+  renderButtons();
 });
